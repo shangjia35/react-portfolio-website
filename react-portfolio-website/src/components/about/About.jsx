9328fa0b8f6e0bf5fcb3b87cd084acfb3c688063
shangjia35 <xsj827@gmail.com>
2023-02-14 12:56:09 -0500

    Initial Commit

---
+++ react-portfolio-website/src/components/about/About.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import './about.css'
+import ME from '../../assets/mypiczoo.jpg'
+
+const About = () => {
+  return (
+    <section id='about'>
+      <h2>A little bit about me...</h2>
+
+      <div className="container about__container">
+
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image"></img>
+          </div>
+        </div>
+
+        <div className="about__content">
+          <p>
+            I am a junior in Computer Science at Purdue University with minors in Mathematics and Music History & Theory. 
+            My concentration is machine intelligence.
+            I am also an undergraduate teaching assistant in Computer Architecture and Systems Programming.
+          </p>
+          <p>
+            I like music and enjoy playing the piano in my spare time. 
+          </p>
+        </div>
+        
+        
+      </div>
+    </section>
+  )
+}
+
+export default About
---
+++ react-portfolio-website/src/components/experience/Experience.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './experience.css'
+
+const Experience = () => {
+  return (
+    <section id='experience'>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className="purdue-data-mine">
+          
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Experience
